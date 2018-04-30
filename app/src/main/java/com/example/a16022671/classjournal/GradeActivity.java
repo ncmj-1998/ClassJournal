9b9d1caf95456c9e8861760f85db8393b7f79658
16022699 <16022699@myrp.edu.sg>
2018-04-30 14:59:54 +0800

    Sabrina Add Info // Done

--- app/src/main/java/com/example/a16022671/classjournal/GradeActivity.java
+++ app/src/main/java/com/example/a16022671/classjournal/GradeActivity.java
@@ -1,6 +1,7 @@
 package com.example.a16022671.classjournal;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +19,9 @@ public class GradeActivity extends AppCompatActivity {
  ListView lvGrade;
     ArrayAdapter aa;
     ArrayList<Grade> grades;
-    Button btnEmail;
+
+    Button btnInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,58 +29,28 @@ public class GradeActivity extends AppCompatActivity {
 
         lvGrade = (ListView)findViewById(R.id.lvGrade);
 
-        btnEmail = (Button) findViewById(R.id.btnEmail);
-
         // Get the intent
         Intent i = getIntent();
         // Get the Hero object first activity put in Intent
         Module module = (Module) i.getSerializableExtra("module");
         grades = new ArrayList<Grade>();
+        for (int a=0;a<module.getDailyGrade().size();a++){
+                grades.add(new Grade(module.getDailyGrade().get(a)));
 
-       if (module.getDailyGrade() == null){
-
-       }
-        else {
-           for (int a = 0; a < module.getDailyGrade().size(); a++) {
-               grades.add(new Grade(module.getDailyGrade().get(a)));
-           }
-       }
+        }
         aa = new GradeAdapter(this, R.layout.graderow, grades);
         lvGrade.setAdapter(aa);
 
-        btnEmail.setOnClickListener(new View.OnClickListener() {
+        btnInfo = (Button) findViewById(R.id.btnInfo);
+        btnInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                // The action you want this intent to do;
-                // ACTION_SEND is used to indicate sending text
-                Intent email = new Intent(Intent.ACTION_SEND);
-                // Put essentials like email address, subject & body text
-                email.putExtra(Intent.EXTRA_EMAIL,
-                        new String[]{"jason_lim@rp.edu.sg"});
-//                email.putExtra(Intent.EXTRA_SUBJECT,
-//                        "Test Email from C347");
-
-                //retrieving the week and grade
-                String message = "Hi faci, \n" + "Please see my remarks so far, thank you!\n";
-                String week = null;
-                for (int i = 0; i < grades.size(); i++) {
-                    week = String.valueOf(i + 1);
-                    String grade = grades.get(i).getGrade();
-                    message += "\nWeek: " + week + " DG: " + grade;
-                }
-                ;
-
-                email.putExtra(Intent.EXTRA_TEXT,
-                        message);
-                // This MIME type indicates email
-                email.setType("message/rfc822");
-                // createChooser shows user a list of app that can handle
-                // this MIME type, which is, email
-                startActivity(Intent.createChooser(email,
-                        "Choose an Email client :"));
-
+                // Intent to display data
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                // Set the URL to be used.
+                rpIntent.setData(Uri.parse("https://www.rp.edu.sg/SOI/full-time-diplomas/Details/r47"));
+                startActivity(rpIntent);
             }
         });
-
-
-    }}
+    }
+}
